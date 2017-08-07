a11fb7825ebd57d1f35e272a4d0b9248193465a4
hofab <hofab@fabianhof.com>
2017-08-07 18:27:14 -0400

    Fix header includes

--- source/main.cpp
+++ source/main.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
 #include <stdlib.h>
 
-#include "exampleConfig.h"
-#include "example.h"
+#include "example.hpp"
 
 /*
  * Simple main program that demontrates how access
  * CMake definitions (here the version number) from source code.
  */
 int main() {
-  std::cout << "C++ Boiler Plate v"
-            << PROJECT_VERSION_MAJOR
-            << "."
-            << PROJECT_VERSION_MINOR
-            << std::endl;
-  std::system("cat ../LICENCE");
+  std::cout << "This is the cpp boilerplate build with meson.build!" << std::endl;
+  /* std::system("cat ../LICENCE"); */
 
   // Bring in the dummy class from the example source,
   // just to show that it is accessible from main.cpp.
