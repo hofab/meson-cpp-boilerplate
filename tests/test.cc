feb284c911a333daab3ee7360080dd6cc65ddf29
hofab <hofab@fabianhof.com>
2017-08-08 22:22:04 -0400

    First draft to add gtest. Not yet working.

---
+++ tests/test.cc
@@ -0,0 +1,15 @@
+#include<gtest/gtest.h>
+
+TEST(basic_test, eq_works) {
+    ASSERT_EQ(0, 1-1) << "Equality is broken. Mass panic!";
+}
+
+TEST(basic_test, neq_works) {
+    ASSERT_NE(15, 105) << "Inequal is equal. The foundations of space and time are in jeopardy.";
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
